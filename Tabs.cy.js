63f5d5324c916ce75c7760f6d3772ef688d0eb64
ulianadzerzhinskaia <43252936+ulianadzerzhinskaia@users.noreply.github.com>
2023-10-16 22:05:44 +0200

    Add files via upload

---
+++ Tabs.cy.js
@@ -0,0 +1,20 @@
+describe('Handle tabs', (()=>{
+    it.skip('Approach 1', ()=>{
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('.example > a').invoke('removeAttr', 'target').click()
+        cy.url().should('include', 'https://the-internet.herokuapp.com/windows/new')
+        cy.wait('5000');
+
+        cy.go('back');
+    })
+    it('Approach 2', ()=>{
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.get('.example > a').then((e)=>{
+            let url=e.prop('href');
+            cy.visit(url);
+        })
+        cy.url().should('include', 'https://the-internet.herokuapp.com/windows/new')
+        
+    })
+
+}))
